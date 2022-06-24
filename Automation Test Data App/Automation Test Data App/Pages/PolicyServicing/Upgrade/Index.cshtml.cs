6a0535e4e7bca5f2970e61b51f17786b886e49b6
Lesedisanlam <Lesedi@sanlamsky.co.za>
2022-06-24 09:15:30 +0200

    commit

--- Automation Test Data App/Automation Test Data App/Pages/PolicyServicing/Upgrade/Index.cshtml.cs
+++ Automation Test Data App/Automation Test Data App/Pages/PolicyServicing/Upgrade/Index.cshtml.cs
@@ -6,49 +6,6 @@ namespace Automation_Test_Data_App.Pages.PolicyServicing.Upgrade
 {
     public class IndexModel : PageModel
     {
-        public List<UpgradeInfo> ListUpgrade= new List<UpgradeInfo>();
-        public void OnGet()
-        {
-            try
-            { 
-              String connectionString = "Data Source='SRV007232, 1455';Initial Catalog=Automation;Integrated Security=True";
-                using (SqlConnection connection = new SqlConnection (connectionString))
-                {
-
-                    connection.Open();
-                    String sql = "SELECT * FROM UpGrade";
-                    using (SqlCommand command = new SqlCommand(sql, connection))
-                    {
-                        using(SqlDataReader reader = command.ExecuteReader()) 
-                        while(reader.Read())
-                         {
-
-                                UpgradeInfo UpgradeInfo = new UpgradeInfo();
-                                UpgradeInfo.id =""+ reader.GetInt32(0);
-                                UpgradeInfo.Date= reader.GetString(1);
-                                UpgradeInfo.Component = reader.GetString(2);
-                                UpgradeInfo.Cover_Amount = reader.GetString(3);
-
-
-                                ListUpgrade.Add(UpgradeInfo);
-                         }
-
-
-                    }
-
-                }
-            
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Exception:" + ex.ToString());
-
-            }
-
-
-
-
-        }
     }
 
     public class UpgradeInfo
