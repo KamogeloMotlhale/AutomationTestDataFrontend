e3e5387402f3c1399b5d40d090e6d3624ca44d67
Lesedisanlam <Lesedi@sanlamsky.co.za>
2022-06-28 05:46:40 +0200

    commit

--- Automation Test Data App/Automation Test Data App/Pages/PolicyServicing/Downgrade/Index.cshtml.cs
+++ Automation Test Data App/Automation Test Data App/Pages/PolicyServicing/Downgrade/Index.cshtml.cs
@@ -5,11 +5,53 @@ using System.Data.SqlClient;
 namespace Automation_Test_Data_App.Pages.PolicyServicing.Downgrade
 {
     public class IndexModel : PageModel
+    { 
+    public List<DowngradeInfo> ListDowngradeComponent = new List<DowngradeInfo>();
+    public void OnGet()
     {
+        try
+        {
+            String connectionString = "Data Source='SRV007232, 1455';Initial Catalog=Automation;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                connection.Open();
+                String sql = "SELECT * FROM Downgrade ";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                        while (reader.Read())
+                        {
+
+                            DowngradeInfo DowngradeInfo = new DowngradeInfo();
+                            DowngradeInfo.id = "" + reader.GetInt32(0);
+                            DowngradeInfo.Date = reader.GetString(1);
+                            DowngradeInfo.Component = reader.GetString(2);
+                            DowngradeInfo.Cover_Amount = reader.GetString(3);
+
+
+
+                            ListDowngradeComponent.Add(DowngradeInfo);
+                        }
+
+
+                }
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception:" + ex.ToString());
+
+        }
+
+
+
+
     }
-
-
-    public class DowngradeInfo
+}
+public class DowngradeInfo
     {
         public String id;
         public String Date;
