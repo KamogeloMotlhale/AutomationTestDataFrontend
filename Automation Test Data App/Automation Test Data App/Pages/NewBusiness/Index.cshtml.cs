a59df56a57b4e666c8f68a74c1dd8442b34e923f
KamogeloMotlhale <92598061+KamogeloMotlhale@users.noreply.github.com>
2022-09-06 16:18:22 +0200

    push

--- Automation Test Data App/Automation Test Data App/Pages/NewBusiness/Index.cshtml.cs
+++ Automation Test Data App/Automation Test Data App/Pages/NewBusiness/Index.cshtml.cs
@@ -1,17 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
 using System.Data.OleDb;
 using System.Data.SqlClient;
 
 namespace Automation_Test_Data_App.Pages.NewBusiness
 {
-
-    
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private string fullPath = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "UploadFiles";
+        private string fullPath = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "UploadedFiles";
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -38,8 +37,52 @@ namespace Automation_Test_Data_App.Pages.NewBusiness
             }
 
             // Process uploaded files  
-            // Don't rely on or trust the FileName property without validation.  
-            ViewData["SuccessMessage"] = formFile.FileName.ToString() + " files uploaded!!";
+            // Don't rely on or trust the FileName property without validation.
+            var connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + filePath + "';Extended Properties=\"Excel 12.0;HDR=YES;\"";
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                try
+                {
+
+                    // Open connection
+                    conn.Open();
+                    string cmdQuery = $"SELECT * FROM [Scenarios$]";
+
+                    OleDbCommand cmd = new OleDbCommand(cmdQuery, conn);
+
+                    // Create new OleDbDataAdapter
+                    OleDbDataAdapter oleda = new OleDbDataAdapter();
+
+                    oleda.SelectCommand = cmd;
+
+                    // Create a DataSet which will hold the data extracted from the worksheet.
+                    DataSet ds = new DataSet();
+
+                    // Fill the DataSet from the data extracted from the worksheet.
+                    oleda.Fill(ds, "Policies");
+
+
+
+                    foreach (var row in ds.Tables[0].DefaultView)
+                    {
+
+                        var excellAge = ((System.Data.DataRowView)row).Row.ItemArray[0].ToString();
+
+                      }
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    conn.Close();
+                    conn.Dispose();
+                }
+
+
+            }
             return Page();
         }
 
